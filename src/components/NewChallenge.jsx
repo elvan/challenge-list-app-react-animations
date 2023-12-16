f92c5fc81ace65a9b4a8cb490bfbb19b8b82aa1f
Elvan Hidayat <elvanhd@gmail.com>
2023-12-17 01:12:02 +0700

    variants and nested animations

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import { useContext, useRef, useState } from 'react';
 
 import images from '../assets/images.js';
@@ -58,13 +59,19 @@ export default function NewChallenge({ onDone }) {
 
         <ul id='new-challenge-images'>
           {images.map((image) => (
-            <li
+            <motion.li
+              variants={{
+                hidden: { opacity: 0, scale: 0.5 },
+                visible: { opacity: 1, scale: 1 },
+              }}
+              exit={{ opacity: 1, scale: 1 }}
+              transition={{ type: 'spring' }}
               key={image.alt}
               onClick={() => handleSelectImage(image)}
               className={selectedImage === image ? 'selected' : undefined}
             >
               <img {...image} />
-            </li>
+            </motion.li>
           ))}
         </ul>
 
